a76af4806737453f1be53b74228064b89133152d
rf777rf777 <rf777rf777@gmail.com>
2024-11-06 23:30:42 +0800

    change yolov8_model to use onnx

--- source/modules/models/yolov8/yolov8_model.py
+++ source/modules/models/yolov8/yolov8_model.py
@@ -1,38 +1,38 @@
-import torch, io
-from modules.models.yolov8.Yolov8DetectionResult import Yolov8DetectionItem, Yolov8DetectionResult
-from ultralytics import YOLO
+import io
+import onnxruntime as ort
+import numpy as np
 from PIL import Image
+from modules.models.yolov8.Yolov8DetectionResult import Yolov8DetectionItem, Yolov8DetectionResult
 
-# check if you can use gpu
-device = 'cuda' if torch.cuda.is_available() else 'cpu'
+session = ort.InferenceSession("modules/models/yolov8/yolov8n.onnx")
+input_name = session.get_inputs()[0].name
 
 def __detect(contents: io.BytesIO, conf: float):
     image = Image.open(contents).convert('RGB')
+    image = image.resize((640, 640))
+    img_np = np.array(image).astype(np.float32)
+    img_np = img_np / 255.0
+    img_np = np.transpose(img_np, (2, 0, 1))
+    img_np = np.expand_dims(img_np, axis=0)
+
+    outputs = session.run(None, {input_name: img_np})
+    result = outputs[0][0]
     
-    # Load the YOLO model
-    model = YOLO("modules/models/yolov8/yolov8n.pt").to(device)
-    results = model.predict(image, conf=conf) 
-
-    if device == 'cuda':
-        # 確保數據被從 GPU 移動到 CPU
-        result = results[0].cpu()
-
-        # 刪除模型以釋放資源
-        del model    
-        # 清理 GPU 緩存
-        torch.cuda.empty_cache()
-    else:
-        result = results[0]
-
-    return  result 
+    detections = []
+    for box in result:
+        if box[4] >= conf:  #Confidence threshold
+            x_center, y_center, width, height = box[:4]
+            class_id = int(box[5])
+            confidence = float(box[4])
+            detections.append(
+                Yolov8DetectionItem(
+                    className=str(class_id),  
+                    confidence=confidence,
+                    box=[[x_center, y_center, width, height]]
+                )
+            )
+    return detections
 
 def get_detect_result_info(contents: io.BytesIO, conf=0.5) -> Yolov8DetectionResult:
-    result = __detect(contents, conf)
-    detections = []
-    for box in result.boxes:
-        detections.append(
-            Yolov8DetectionItem(
-                className=result.names[int(box.cls)], 
-                confidence=float(box.conf), 
-                box=box.xywh.tolist()))
+    detections = __detect(contents, conf)
     return Yolov8DetectionResult(detections=detections)
